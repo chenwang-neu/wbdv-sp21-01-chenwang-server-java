d116def638c9d91aad89c02f132e5858d2aaea34
chen wang <wangchen.neu@gmail.com>
2021-02-01 01:33:09 -0500

    create login register and profile page

---
+++ src/test/java/com/example/wbdvsp2101wangchenserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2101wangchenserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
